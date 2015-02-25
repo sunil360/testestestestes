43c95728d19ec301f4a74b5123e87f22c1980b2a
unknown-user <unknown-user@10.211.55.2>
2015-02-25 20:58:04 +0530

    Initial check-in of module asdasdasdqewqewe

---
+++ asdasdasdqewqewe/AppDelegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+
+namespace asdasdasdqewqewe
+{
+	public partial class AppDelegate : NSApplicationDelegate
+	{
+		MainWindowController mainWindowController;
+
+		public AppDelegate ()
+		{
+		}
+
+		public override void DidFinishLaunching (NSNotification notification)
+		{
+			mainWindowController = new MainWindowController ();
+			mainWindowController.Window.MakeKeyAndOrderFront (this);
+		}
+	}
+}
+
